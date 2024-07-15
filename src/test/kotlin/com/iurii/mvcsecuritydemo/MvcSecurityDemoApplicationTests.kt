251db28966cddac7f53f9cfdec87f47c94d720de
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-15 16:25:58 +0200

    Initial commit

---
+++ src/test/kotlin/com/iurii/mvcsecuritydemo/MvcSecurityDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.iurii.mvcsecuritydemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MvcSecurityDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
