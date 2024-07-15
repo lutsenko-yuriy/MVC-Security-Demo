2b4428b124e4c00ba9c8b9762340f53947a08d92
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-15 16:51:22 +0200

    Add Demo security configuration

--- src/main/kotlin/com/iurii/mvcsecuritydemo/security/DemoSecurityConfiguration.kt
+++ src/main/kotlin/com/iurii/mvcsecuritydemo/security/DemoSecurityConfiguration.kt
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.provisioning.UserDetailsManager
 
 @Configuration
 class DemoSecurityConfiguration {
 
     @Bean
-    fun inMemoryUserDetailsManager(): InMemoryUserDetailsManager {
+    fun userDetailsManager(): UserDetailsManager {
         val john = User.builder()
             .username("john")
             .password("{noop}Test123")
