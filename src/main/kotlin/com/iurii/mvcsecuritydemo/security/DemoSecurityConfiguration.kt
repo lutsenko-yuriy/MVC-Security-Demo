31fea67501ce26309529d96699f9d56cf763e5ad
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-15 17:11:11 +0200

    Add login controller and form

--- src/main/kotlin/com/iurii/mvcsecuritydemo/security/DemoSecurityConfiguration.kt
+++ src/main/kotlin/com/iurii/mvcsecuritydemo/security/DemoSecurityConfiguration.kt
@@ -2,12 +2,27 @@ package com.iurii.mvcsecuritydemo.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.provisioning.UserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 class DemoSecurityConfiguration {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeHttpRequests {
+            it.anyRequest().authenticated()
+        }
+            .formLogin {
+                it.loginPage("/login")
+                    .loginProcessingUrl("/authenticateTheUser")
+                    .permitAll()
+            }
+        return http.build()
+    }
 
     @Bean
     fun userDetailsManager(): UserDetailsManager {
